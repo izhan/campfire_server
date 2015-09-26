ee15ecf81b118bb455a13338f0e40af7c12f89fb
Irvin Zhan <izhan@princeton.edu>
2015-09-26 19:08:17 -0400

    adding postmessage as redirect-uri (ugh obscure google documentation)

--- app/controllers/users/gplus_oauth_callback_controller.rb
+++ app/controllers/users/gplus_oauth_callback_controller.rb
@@ -1,18 +1,23 @@
 require 'google/api_client/client_secrets'
 
+# TODO this should not be here
 CLIENT_SECRETS_FILE = Rails.root.join('config', 'oauth', 'gplus_client_secret.json')
 
 class Users::GplusOauthCallbackController < ApplicationController
   def callback
-    puts "in callback"
+    puts "in callbaddsckd"
+    puts params
+    puts params[:code]
     if params[:code]
       puts "within if"
       client_secrets = Google::APIClient::ClientSecrets.load(CLIENT_SECRETS_FILE)
       authorization = client_secrets.to_authorization
       authorization.scope = 'https://www.googleapis.com/auth/calendar'
       authorization.code = params[:code]
+      authorization.redirect_uri = "postmessage"
 
       puts "here"
+      puts authorization
       asdf = authorization.fetch_access_token!
       puts "there"
       puts asdf
