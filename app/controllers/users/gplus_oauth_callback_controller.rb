e1ca16b57655804c2a0994c96c12428e61e11196
Irvin Zhan <izhan@princeton.edu>
2015-09-26 20:21:41 -0400

    on callback, fetch user info and create new user if needed

--- app/controllers/users/gplus_oauth_callback_controller.rb
+++ app/controllers/users/gplus_oauth_callback_controller.rb
@@ -1,4 +1,5 @@
 require 'google/api_client/client_secrets'
+require 'google/api_client'
 
 # TODO this should not be here
 CLIENT_SECRETS_FILE = Rails.root.join('config', 'oauth', 'gplus_client_secret.json')
@@ -15,12 +16,20 @@ class Users::GplusOauthCallbackController < ApplicationController
       authorization.scope = 'https://www.googleapis.com/auth/calendar'
       authorization.code = params[:code]
       authorization.redirect_uri = "postmessage"
+      authorization.fetch_access_token!
 
-      puts "here"
-      puts authorization
-      asdf = authorization.fetch_access_token!
-      puts "there"
-      puts asdf
+      access_token = authorization.access_token
+
+      user = User.find_or_create_by(access_token: access_token)
+      
+      # using Fred Guest's stateless API strategy
+      # http://fredguest.com/2015/03/06/building-a-stateless-rails-api-with-react-and-twitter-oauth/
+      jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
+      
+      # is this okay?
+      render json: { jwt: jwt }
+    else
+      render json: 'oauth auth failed', status: 401
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ActiveRecord::Base
+  before_create :add_uid
+
+  private
+    # kind of sucks that the id is not returned to us 
+    # in oauth authentication
+    def fetch_user_info(access_token)
+      # TODO shouldn't have to initialize this every time
+      client = Google::APIClient.new(
+        application_name: "Campfire",
+        application_version: "0.0.1"
+      )
+
+      client.authorization.access_token = access_token
+      service = client.discovered_api('plus', 'v1')
+      puts "before execute"
+      google_response = client.execute(
+        :api_method => service.people.get,
+        :parameters => { userId: "me" },
+        :headers => {'Content-Type' => 'application/json'}
+      )
+      puts "after execute"
+      JSON.parse(google_response.body)
+      
+    end
+
+    def add_user_info
+      user_info = fetch_user_info(self.access_token)
+      self.uid = user_info["id"]
+      # TODO and if they have more than one email?  is the first one always
+      # the primary one?
+      self.email = user_info["emails"][0]["value"]
+    end
 end
