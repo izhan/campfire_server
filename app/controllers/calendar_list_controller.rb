f3e8d625dd4f9a19817eaaedd02cc68bc5067406
Irvin Zhan <izhan@princeton.edu>
2015-09-27 02:38:08 -0400

    WIP adding calendar list sync on initialize

--- app/controllers/calendar_list_controller.rb
+++ app/controllers/calendar_list_controller.rb
@@ -2,6 +2,10 @@ class CalendarListController < BaseApiController
   before_action :authenticate_request, only: [:index]
 
   def index
-    @current_user
+    if @current_user.calendar_list
+      render json: @current_user.calendar_list
+    else
+      @current_user.create_calendar_list()
+    end
   end
 end
--- app/models/calendar_list.rb
+++ app/models/calendar_list.rb
@@ -1,3 +1,29 @@
+require 'google/api_client'
+
 class CalendarList < ActiveRecord::Base
+  before_create :initial_sync
   belongs_to :user
+
+  serialize :json_data
+
+  private
+
+    def initial_sync
+      puts "initial sync"
+      client = Google::APIClient.new(
+        application_name: "Campfire",
+        application_version: "0.0.1"
+      )
+      client.authorization.access_token = self.user.access_token
+      service = client.discovered_api('calendar', 'v3')
+      result = client.execute(
+        :api_method => service.calendar_list.list,
+        :parameters => {},
+        :headers => {'Content-Type' => 'application/json'}
+      )
+
+      # calendars could paginate too, though we'll assume that
+      # the calendarlist isnt that long
+      self.json_data = result.data.items
+    end
 end
