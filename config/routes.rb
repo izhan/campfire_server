106603b9c102775ded173d91081921ed7e76f0c5
Irvin Zhan <izhan@princeton.edu>
2015-09-26 22:13:54 -0400

    namespace to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :calendar_list, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :calendar_list, only: [:index]
+    end
+  end
+  
 
   post '/auth/google_oauth2/callback' => 'users/gplus_oauth_callback#callback'
 end
